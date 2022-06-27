af8285a8d821a3d8716a6422ae1f17732465614d
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-06-27 09:17:27 +0600

    changed

--- index.js
+++ index.js
@@ -52,6 +52,23 @@ async function run() {
             const result = await spiceCollection.updateOne(filter, updatedDoc, options);
             res.send(result)
         })
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const newQuantity = req.body.quantity;
+            console.log(newQuantity);
+
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: newQuantity,
+
+                }
+            }
+            console.log(updatedDoc);
+            const result = await spiceCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
 
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
